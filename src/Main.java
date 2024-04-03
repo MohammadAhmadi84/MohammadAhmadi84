61ac15028b3f8056bd1dd2f3ce1845a05a5a6757
mamali <mohammad.a.1384.per@gmail.com>
2024-04-03 04:54:11 -0700

    ddf

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,7 @@ final class DataBase {
     private ArrayList<UserAccountModel>[] Users = new ArrayList[5];
     private ArrayList<AudioModel>[] Audios = new ArrayList[2];
     private ArrayList<ReportModel> Reports = new ArrayList<>();
+    private ArrayList<PlayListModel>playLists=new ArrayList<>();
 
     public void setUsers(UserAccountModel users) {
         if (users instanceof AdminModel) {
@@ -54,6 +55,26 @@ final class DataBase {
         }
     }
 
+    public void setPlayLists(PlayListModel playLists) {
+        this.playLists.add(this.playLists.size(),playLists);
+    }
+    public String getPlayLists(){
+        String result="";
+        for (PlayListModel playlist:playLists){
+            result+=playlist.toString()+"\n";
+        }
+        return result;
+    }
+    public String getPlayLists(String playlistName){
+        String result="";
+        for (PlayListModel playlist:playLists){
+            if (playlist.getPlaylistName().contains(playlistName)){
+                result+=playlist.toString()+"\n"+playlist.getAudio()+"\n";
+            }
+        }
+        return result;
+    }
+
     public void setReports(ReportModel reports) {
         Reports.add(Reports.size(), reports);
     }
@@ -61,15 +82,15 @@ final class DataBase {
     public boolean checkListener(UserAccountModel listener) {
         for (UserAccountModel a : Users[1]) {
             if (a.getUserName().equals(listener.getUserName())) {
-                return false;
+                return true;
             }
         }
         for (UserAccountModel a : Users[2]) {
             if (a.getUserName().equals(listener.getUserName())) {
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     public int findListener(String username, String password) {
@@ -218,6 +239,26 @@ final class DataBase {
         }
 
     }
+    public void sortByPlaying(){
+        for (int i=0;i<Audios[0].size()-1;i++){
+            for (int j=0;j<Audios[0].size()-i-1;j++){
+                if (Audios[0].get(j).getPlayCounter()<Audios[0].get(j+1).getLikeCounter()){
+                    AudioModel tmp=Audios[0].get(j);
+                    Audios[0].add(j,Audios[0].get(j+1));
+                    Audios[0].add(j+1,Audios[0].get(j));
+                }
+            }
+        }
+        for (int i=0;i<Audios[1].size()-1;i++){
+            for (int j=0;j<Audios[1].size()-i-1;j++){
+                if (Audios[1].get(j).getPlayCounter()<Audios[1].get(j+1).getPlayCounter()){
+                    AudioModel tmp=Audios[1].get(j);
+                    Audios[1].add(j,Audios[1].get(j+1));
+                    Audios[1].add(j+1,Audios[1].get(j));
+                }
+            }
+        }
+    }
 }
 
 enum Genre {
@@ -300,10 +341,19 @@ abstract class UserAccountModel {
         return dateModifier;
     }
 
+    public Date getBirthday() {
+        return birthday;
+    }
+
     @Override
     public String toString() {
         return "UserAccountModel{" +
-                "fullName='" + fullName + '\'' +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", birthday=" + birthday +
                 ", dateModifier='" + dateModifier + '\'' +
                 '}';
     }
@@ -526,14 +576,24 @@ final class PlayListModel {
     public PlayListModel(String playlistName, String makerName) {
         this.playlistName = playlistName;
         this.makerName = makerName;
-        this.playListIdentity = String.valueOf(playlistName.length()) + String.valueOf(makerName.length()) + "/" + String.valueOf(PlayListModel.playlistCounter);
+        PlayListModel.playlistCounter++;
+        this.playListIdentity = String.valueOf(playlistName.length()) + String.valueOf(makerName.length())+ String.valueOf(PlayListModel.playlistCounter);
     }
 
     private String playListIdentity;
     private String playlistName;
     private String makerName;
     ArrayList<AudioModel> audio = new ArrayList<>();
-    private static long playlistCounter;
+    private static long playlistCounter=0;
+
+    @Override
+    public String toString() {
+        return "PlayListModel{" +
+                "playListIdentity='" + playListIdentity + '\'' +
+                ", playlistName='" + playlistName + '\'' +
+                ", makerName='" + makerName + '\'' +
+                '}';
+    }
 
     public String showAudio() {
         String result = "";
@@ -558,15 +618,26 @@ final class PlayListModel {
     public static long getPlaylistCounter() {
         return playlistCounter;
     }
+    public long getNumberOfAudio(){
+        return audio.size();
+    }
+    public String getAudio(){
+        String result="";
+        for (AudioModel audioModel:audio){
+            result+=audioModel.toString()+"\n";
+        }
+        return result;
+    }
+    public void setAudio(AudioModel audio){
+        this.audio.add(this.audio.size(),audio);
+    }
 }
 
 abstract class ListenerModel extends UserAccountModel {
-    public ListenerModel(String userName, String password, String fullName, String email, String phoneNumber, String dateModifier, Date expiryDate, ArrayList<Genre> favoriteGenre) {
+    public ListenerModel(String userName, String password, String fullName, String email, String phoneNumber, String dateModifier, Date expiryDate) {
         super(userName, password, fullName, email, phoneNumber, dateModifier);
         this.userAccountCredentials = 50;
         this.expiryDate = expiryDate;
-        this.favoriteGenre = favoriteGenre;
-
     }
 
 
@@ -575,10 +646,27 @@ abstract class ListenerModel extends UserAccountModel {
     private ArrayList<Genre> favoriteGenre = new ArrayList<>();
     private Map<AudioModel, Long> musicPlayCounter = new HashMap<>();
     private ArrayList<ArtistModel> following = new ArrayList<>();
+    private ArrayList<PlayListModel>playLists=new ArrayList<>();
+    public String getPlaylists(){
+        String result="";
+        for (PlayListModel playlist:playLists){
+            result+=playlist.toString()+"\n";
+        }
+        return result;
+    }
+    public PlayListModel findPlayList(String playlistId){
+        for (PlayListModel playListModel:playLists){
+            if (playlistId.equals(playListModel.getPlayListIdentity())){
+                return playListModel;
+            }
+        }
+        return null;
+    }
 
     public double getUserAccountCredentials() {
         return userAccountCredentials;
     }
+
 
     public Date getExpiryDate() {
         return expiryDate;
@@ -596,19 +684,31 @@ abstract class ListenerModel extends UserAccountModel {
         }
         return tmp;
     }
+
+    public void setUserAccountCredentials(double userAccountCredentials) {
+        this.userAccountCredentials = userAccountCredentials;
+    }
+
+    public void setFavoriteGenre(ArrayList<Genre> favoriteGenre) {
+        this.favoriteGenre = favoriteGenre;
+    }
+
+    public ArrayList<Genre> getFavoriteGenre() {
+        return favoriteGenre;
+    }
 }
 
 final class FreeListenerModel extends ListenerModel {
-    public FreeListenerModel(String userName, String password, String fullName, String email, String phoneNumber, String dateModifier, ArrayList<Genre> favoriteGenre, int musicAddCounter, int playlistCounter) {
-        super(userName, password, fullName, email, phoneNumber, dateModifier, null, favoriteGenre);
+    public FreeListenerModel(String userName, String password, String fullName, String email, String phoneNumber, String dateModifier, int musicAddCounter, int playlistCounter) {
+        super(userName, password, fullName, email, phoneNumber, dateModifier, null);
         this.musicAddCounter = musicAddCounter;
         this.playlistCounter = playlistCounter;
     }
 
-    private final static long MusicAddLimit = 100;
-    private final static long playListMakeLimit = 20;
-    private int musicAddCounter;
-    private int playlistCounter;
+    private final static long MusicAddLimit = 10;
+    private final static long playListMakeLimit = 3;
+    private int musicAddCounter=0;
+    private int playlistCounter=0;
 
     public int getMusicAddCounter() {
         return musicAddCounter;
@@ -626,11 +726,26 @@ final class FreeListenerModel extends ListenerModel {
         return FreeListenerModel.MusicAddLimit;
     }
 
+    public void setMusicAddCounter(int musicAddCounter) {
+        this.musicAddCounter = musicAddCounter;
+    }
+
+    public void setPlaylistCounter(int playlistCounter) {
+        this.playlistCounter = playlistCounter;
+    }
+
+    @Override
+    public String toString() {
+        return "FreeListenerModel{" +
+                "musicAddCounter=" + musicAddCounter +
+                ", playlistCounter=" + playlistCounter +
+                '}';
+    }
 }
 
 final class PremiumListenerModel extends ListenerModel {
-    public PremiumListenerModel(String userName, String password, String fullName, String email, String phoneNumber, String dateModifier, Date expiryDate, ArrayList<Genre> favoriteGenre) {
-        super(userName, password, fullName, email, phoneNumber, dateModifier, expiryDate, favoriteGenre);
+    public PremiumListenerModel(String userName, String password, String fullName, String email, String phoneNumber, String dateModifier, Date expiryDate) {
+        super(userName, password, fullName, email, phoneNumber, dateModifier, expiryDate);
     }
 
     private long remainingDys;
@@ -651,9 +766,9 @@ class ListenerController {
         return listenerController;
     }
 
-    public boolean signIn(String userName, String password, String fullName, String email, String phoneNumber, String dateModifier, ArrayList<Genre> favoriteGenre, int musicAddCounter, int playlistCounter) {
-        FreeListenerModel tmp = new FreeListenerModel(userName, password, fullName, email, phoneNumber, dateModifier, favoriteGenre, musicAddCounter, playlistCounter);
-        if (DataBase.getDataBase().checkListener(tmp)) {
+    public boolean signIn(String userName, String password, String fullName, String email, String phoneNumber, String dateModifier, int musicAddCounter, int playlistCounter) {
+        FreeListenerModel tmp = new FreeListenerModel(userName, password, fullName, email, phoneNumber, dateModifier, musicAddCounter, playlistCounter);
+        if (!(DataBase.getDataBase().checkListener(tmp))) {
             DataBase.getDataBase().setUsers(tmp);
             return true;
         }
@@ -702,8 +817,123 @@ class ListenerController {
         return listenerModel.getFollowing();
     }
     public String sortByLike(){
+        DataBase.getDataBase().sortByLike();
+        return DataBase.getDataBase().audioToString();
+    }
+    public String sortByPlay(){
+        DataBase.getDataBase().sortByPlaying();
+        return DataBase.getDataBase().audioToString();
+    }
+    public boolean MakePlaylist(ListenerModel listener,String playListName){
+        return true;
+    }
+    public boolean addMusic(ListenerModel listener,String playListId,String AudioId){
+        return true;
+    }
+
+    public String showPlayList(String playListName){
+        return DataBase.getDataBase().getPlayLists(playListName);
+    }
+    public String showUserPlayLists(ListenerModel listener){
+        return listener.getPlaylists();
+    }
+    public boolean creditImprove(ListenerModel listener,double price){
+        if (DataBase.getDataBase().checkListener(listener)){
+            listener.setUserAccountCredentials(listener.getUserAccountCredentials()+price);
+            return true;
+        }
+        return false;
+    }
+    public String getSuggestion(ListenerModel listener){
         String result="";
         return result;
+    }
+    public String showAccountInfo(ListenerModel listenerModel){
+        String result="";
+        result+="UserName: "+listenerModel.getUserName()+"\"
+                +"Password: "+listenerModel.getPassword()+"\"
+                +"Birthday: "+listenerModel.getBirthday()+"\"
+                +"FullName: "+listenerModel.getFullName()+"\"
+                +"Phone number"+listenerModel.getPhoneNumber()+"\"
+                +"Email"+listenerModel.getEmail();
+
+        return result;
+    }
+    public void setFavoriteGenre(ListenerModel listener,ArrayList<String>genre){
+        ArrayList<Genre>genres=new ArrayList<>();
+        for (String userGenre:genre){
+            switch (userGenre){
+                case "HipHop":
+                    genres.add(Genre.HIPHOP);
+                    break;
+                case "Rock":
+                    genres.add(Genre.ROCK);
+                    break;
+                case "Pop":
+                    genres.add(Genre.POP);
+                    break;
+                case "Jazz":
+                    genres.add(Genre.JAZZ);
+                    break;
+                case "Country":
+                    genres.add(Genre.COUNTRY);
+                    break;
+                case "True Crime":
+                    genres.add(Genre.TRUE_CRIME);
+                    break;
+                case "Society":
+
+            }
+        }
+    }
+}
+class FreeListenerController extends ListenerController{
+    @Override
+    public boolean MakePlaylist(ListenerModel listener,String playListName) {
+        FreeListenerModel tmp=(FreeListenerModel) listener;
+        if (tmp.getPlaylistCounter()<3) {
+            PlayListModel newPlayList = new PlayListModel(playListName, listener.getFullName());
+            tmp.setPlaylistCounter(tmp.getPlaylistCounter()+1);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addMusic(ListenerModel listener, String playListId,String AudioId) {
+        PlayListModel tmpPlaylist=listener.findPlayList(playListId);
+        FreeListenerModel tmpListener=(FreeListenerModel) listener;
+        if (tmpPlaylist==null){
+            return false;
+        }
+        if (tmpPlaylist.getNumberOfAudio()<10){
+            if (DataBase.getDataBase().getAudio(AudioId)!=null) {
+                tmpPlaylist.setAudio(DataBase.getDataBase().getAudio(AudioId));
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
+class PremiumController extends ListenerController{
+    @Override
+    public boolean MakePlaylist(ListenerModel listener, String playListName) {
+        PremiumListenerModel tmp=(PremiumListenerModel) listener;
+        PlayListModel newPlayList = new PlayListModel(playListName, listener.getFullName());
+        return true;
+    }
+
+    @Override
+    public boolean addMusic(ListenerModel listener, String playListId, String AudioId) {
+        PremiumListenerModel tmpListener=(PremiumListenerModel) listener;
+        PlayListModel tmpPlaylist=listener.findPlayList(playListId);
+        if (tmpPlaylist!=null && DataBase.getDataBase().getAudio(AudioId)!=null){
+            tmpPlaylist.setAudio(DataBase.getDataBase().getAudio(AudioId));
+            return true;
+        }
+
+        return false;
     }
 }
 
